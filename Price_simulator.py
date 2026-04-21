26071893a9ad8817b16d32d6de73703a4c0b2616
Fahadalinsn <fahadch6888@gmail.com>
2026-04-21 09:25:39 +0500

    Added Short_term  Price_simulator code

---
+++ Price_simulator.py
@@ -0,0 +1,5 @@
+import numpy as np
+from numpy import random
+price=np.random.normal(73000,72000,10)
+print("simulate :", price)
+print("mean :" ,np.mean(price),"max value :",np.max(price),"min value :",np.min(price))
